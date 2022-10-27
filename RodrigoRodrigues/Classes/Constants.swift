3804becaf6a5ef5c6dace551637cee3c86919b40
Rodrigo Lourenço <>
2022-10-27 00:00:50 -0300

    Removing duplicated import.

--- RodrigoRodrigues/Classes/Constants.swift
+++ RodrigoRodrigues/Classes/Constants.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-import Foundation
-
 struct Constants {
     static let clientID = "c608c8a88fe0802"
     static let apiURL = "https://api.imgur.com/3/gallery/search/top/top/0?q="
